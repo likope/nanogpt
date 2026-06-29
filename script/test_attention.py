afd40a00286c6c1082151ebbc58ab486b53b6264
Max <massimo.domenici06@gmail.com>
2026-06-29 18:24:04 +0200

    update attention

--- script/test_attention.py
+++ script/test_attention.py
@@ -1,14 +1,32 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-torch.manual_seed(1337)
-B, T, C = 1, 4, 8
-x = torch.randn(B, T, C)
-head_size = 4
-key = nn.Linear(C, head_size, bias=False)
+"""
+File per capire come funziona l attention.
+"""
+
+torch.manual_seed(1337) #imposto un seed
+B, T, C = 1, 4, 8 #imposto dei parametri fittizi
+x = torch.randn(B, T, C) #genero una matrice casuale
+head_size = 4 #parametro
+key = nn.Linear(C, head_size, bias=False) #eseguo un operazione lineare ma senza bias, dato che voglio solo gli scalari
 query = nn.Linear(C, head_size, bias=False)
 value = nn.Linear(C, head_size, bias=False)
-k = key(x)
+k = key(x) #chiamo le operazioni lineari
 q = query(x)
 v = value(x)
-print(x.shape, k.shape, q.shape, v.shape)
+
+score = q@k.transpose(-2, -1) #ottengo una matrice tokenxtoken
+scores = score * (head_size ** -0.5)
+
+A = torch.ones(1, 4, 4)
+A = torch.tril(A)
+
+scores = scores.masked_fill(A == 0, float('-inf'))
+wei = F.softmax(scores, dim=-1)
+out = wei @ v
+
+print(out.shape)
+print(wei[0].sum(dim=-1))
+print(wei[0])
