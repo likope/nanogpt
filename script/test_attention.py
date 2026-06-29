d2e62b66d7e4185091999328e4c431b1cb45470b
Max <massimo.domenici06@gmail.com>
2026-06-29 21:59:11 +0200

    attention finito

--- script/test_attention.py
+++ script/test_attention.py
@@ -9,24 +9,33 @@ File per capire come funziona l attention.
 torch.manual_seed(1337) #imposto un seed
 B, T, C = 1, 4, 8 #imposto dei parametri fittizi
 x = torch.randn(B, T, C) #genero una matrice casuale
-head_size = 4 #parametro
-key = nn.Linear(C, head_size, bias=False) #eseguo un operazione lineare ma senza bias, dato che voglio solo gli scalari
-query = nn.Linear(C, head_size, bias=False)
-value = nn.Linear(C, head_size, bias=False)
-k = key(x) #chiamo le operazioni lineari
-q = query(x)
-v = value(x)
 
-score = q@k.transpose(-2, -1) #ottengo una matrice tokenxtoken
-scores = score * (head_size ** -0.5)
+#creazione della classe dell attention
+class Attention(nn.Module):
 
-A = torch.ones(1, 4, 4)
-A = torch.tril(A)
+    def __init__(self, head_size):
+        """Costruttore non dinamico che inizializza le risorse"""
+        super().__init__() #chiamata al costruttore della classe madre
+        self.head_size = head_size
+        self.query = nn.Linear(C, head_size, bias=False) 
+        self.key = nn.Linear(C, head_size, bias=False)
+        self.value = nn.Linear(C, head_size, bias=False) #definisco le operazioni lineari, con numeri casuali all inizializzazione, queste non hanno bias dato che sono degli scalari
+        self.register_buffer('tril', torch.tril(torch.ones(T, T))) #registro il buffer, questo crea una matrice di uni
 
-scores = scores.masked_fill(A == 0, float('-inf'))
-wei = F.softmax(scores, dim=-1)
-out = wei @ v
-
-print(out.shape)
-print(wei[0].sum(dim=-1))
-print(wei[0])
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x) #chiamo le operazioni lineari
+        q = self.query(x)
+        v = self.value(x)
+        score = q@k.transpose(-2, -1) #ottengo una matrice tokenxtoken
+        scores = score * (self.head_size ** -0.5) #ammorbidisco i numeri
+        scores = scores.masked_fill(self.tril[:T, :T] == 0, float('-inf')) #metto tutti i numeri della diagonale superiori a 0
+        wei = F.softmax(scores, dim=-1) #applico il softmax
+        out = wei @ v #output
+        return out
+    
+h = Attention(head_size=4)
+out = h(x)
+print(out.shape)                                   # (1, 4, 4)
+print('tril' in dict(h.named_buffers()))           # True
+print('tril' in dict(h.named_parameters()))        # False
